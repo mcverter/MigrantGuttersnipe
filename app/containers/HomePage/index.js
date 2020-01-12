9bf450be3efb3c15ba15af988481564903149321
mcverter <mitchell.verter@gmail.com>
2020-01-12 08:08:51 -0600

    need to fix apache routing.  region links are now /[a-zA-Z]+

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -15,15 +15,10 @@ export default function HomePage() {
       <h1> Guia Migrante </h1>
       <ul style={{ fontSize: '175%' }}>
         <li>
-          <Link to="/Region/Tijuana">Mapa de Recursos en Tijuana, Mexico</Link>
+          <Link to="/Tijuana">Mapa de Recursos en Tijuana, Mexico</Link>
         </li>
         <li>
-          <Link to="/Region/Tapachula">
-            Mapa de Recursos en Tapachula, Mexico
-          </Link>
-        </li>
-        <li>
-          <Link to="/Shareable/44142960">Link to a record </Link>
+          <Link to="/Tapachula">Mapa de Recursos en Tapachula, Mexico</Link>
         </li>
         <li>
           <Link to="/elnumerodelalista">El Numero de la Lista (Tijuana)</Link>
