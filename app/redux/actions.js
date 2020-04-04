101c6f5f83278a47bd09013b7e5242283ce2d6ff
mcverter <mitchell.verter@gmail.com>
2020-04-04 15:32:00 -0400

    mock json

---
+++ app/redux/actions.js
@@ -0,0 +1,21 @@
+export const REQUEST_ALL_SHAREABLES = 'REQUEST_ALL_SHAREABLES'
+export const RECEIVE_ALL_SHAREABLES = 'RECEIVE_ALL_SHAREABLES'
+
+const SERVER_URL = 'http://foo.com'
+export const requestAllShareables = () => ({
+  type: REQUEST_ALL_SHAREABLES
+})
+
+export const recieveAllShareables = (json) => ({
+  type: RECEIVE_ALL_SHAREABLES,
+  shareables: json.data.shareables,
+})
+
+export const fetchAllShareables = () => dispatch => {
+  dispatch(requestAllShareables())
+  return fetch(SERVER_URL)
+    .then(response => response.json())
+    .then(json => dispatch(recieveAllShareables(json)))
+}
+
+
---
+++ app/redux/reducers.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux'
+
+import {
+  RECEIVE_ALL_SHAREABLES,
+  REQUEST_ALL_SHAREABLES
+} from './actions';
+
+
+const shareablesReducer = (state = {
+  isFetching: false,
+  shareables: []
+}, action) => {
+  switch(action.type) {
+    case RECEIVE_ALL_SHAREABLES:
+      return {
+        ...state,
+        isFetching: false,
+        shareables: action.shareables
+      };
+    case REQUEST_ALL_SHAREABLES:
+      return {
+        ...state,
+        isFetching: true
+      };
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  shareablesReducer
+})
+
+export default rootReducer;
